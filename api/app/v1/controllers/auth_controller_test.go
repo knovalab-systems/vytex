6124a7caeebde63c4d6b88f7b38cf80ec9c5780a
andresNeveu <vordones64@gmail.com>
2024-05-01 23:52:54 -0500

    Test: remake Test
    - split login n refresh test
    - check error in test
    - add new cases

--- api/app/v1/controllers/auth_controller_test.go
+++ api/app/v1/controllers/auth_controller_test.go
@@ -18,62 +18,141 @@ import (
 )
 
 func TestLogin(t *testing.T) {
-	testCases := []struct {
-		Name   string
-		Params map[string]any
-		Code   int
-	}{
-		{Name: "Fail binding", Params: map[string]any{"username": 23232}, Code: http.StatusBadRequest},
-		{Name: "Fail validation, missing parameters ", Params: map[string]any{}, Code: http.StatusBadRequest},
-		{Name: "Fail validation, password length", Params: map[string]any{"username": "jose", "password": "1234567"}, Code: http.StatusBadRequest},
-		{Name: "Not found user", Params: map[string]any{"username": "antonio", "password": "antonio1"}, Code: http.StatusUnauthorized},
-		{Name: "Find invalid password", Params: map[string]any{"username": "jose", "password": "antonio1"}, Code: http.StatusUnauthorized},
-		{Name: "Not generate tokens", Params: map[string]any{"username": "jose", "password": "12345678"}, Code: http.StatusInternalServerError},
-		{Name: "Not save refresh token", Params: map[string]any{"username": "jose", "password": "12345678"}, Code: http.StatusInternalServerError},
-	}
-
-	authorizedUser := &models.User{ID: "1", UserName: "jose", Password: "12345678"}
-	unauthorizedUser := &models.User{UserName: "antonio", Password: "123456789"}
-
-	for i := range testCases {
-		testCase := testCases[i]
-
-		t.Run(testCase.Name, func(t *testing.T) {
-			t.Parallel()
-
-			// context
-			body := new(bytes.Buffer)
-			json.NewEncoder(body).Encode(testCase.Params)
-			req := httptest.NewRequest(http.MethodPost, "/", body)
-			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-			rec := httptest.NewRecorder()
-			e := echo.New()
-			config.EchoValidator(e)
-			c := e.NewContext(req, rec)
-
-			// mocks
-			mockAuth := mocks.AuthMock{}
-			mockAuth.On("UserForLogin", authorizedUser.UserName).Return(authorizedUser, nil)
-			mockAuth.On("UserForLogin", unauthorizedUser.UserName).Return(unauthorizedUser, errors.New("error"))
-			mockAuth.On("RegisterRefresh", authorizedUser.ID, authorizedUser.ID).Return(errors.New("error"))
-			tokenMock := mocks.TokenMock{}
-			tokenMock.On("GenerateTokens", authorizedUser.ID).Return(errors.New("error"))
-
-			// controller
-			controller := AuthController{AuthRepository: &mockAuth, TokensRepository: &tokenMock}
-
-			// test
-			err := controller.Login(c)
-			if assert.Error(t, err) {
-				assert.Equal(t, testCase.Code, err.(*echo.HTTPError).Code)
-			}
-
-		})
-	}
-
-	t.Run("Login successfully", func(t *testing.T) {
-
+	t.Run("Fail binding", func(t *testing.T) {
 		// context
+		body := new(bytes.Buffer)
+		json.NewEncoder(body).Encode(map[string]any{"username": 23232})
+		req := httptest.NewRequest(http.MethodPost, "/", body)
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		e := echo.New()
+		config.EchoValidator(e)
+		c := e.NewContext(req, rec)
+
+		// mocks
+		mockAuth := mocks.AuthMock{}
+		tokenMock := mocks.TokenMock{}
+
+		// controller
+		controller := AuthController{AuthRepository: &mockAuth, TokensRepository: &tokenMock}
+
+		// test
+		err := controller.Login(c)
+		if assert.Error(t, err) {
+			assert.Equal(t, http.StatusBadRequest, err.(*echo.HTTPError).Code)
+		}
+
+	})
+
+	t.Run("Fail validation, missing parameters", func(t *testing.T) {
+		// context
+		req := httptest.NewRequest(http.MethodPost, "/", nil)
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		e := echo.New()
+		config.EchoValidator(e)
+		c := e.NewContext(req, rec)
+
+		// mocks
+		mockAuth := mocks.AuthMock{}
+		tokenMock := mocks.TokenMock{}
+
+		// controller
+		controller := AuthController{AuthRepository: &mockAuth, TokensRepository: &tokenMock}
+
+		// test
+		err := controller.Login(c)
+		if assert.Error(t, err) {
+			assert.Equal(t, http.StatusBadRequest, err.(*echo.HTTPError).Code)
+		}
+
+	})
+
+	t.Run("Fail validation, length password", func(t *testing.T) {
+		// context
+		body := new(bytes.Buffer)
+		json.NewEncoder(body).Encode(map[string]string{"username": "jose", "password": "1234567"})
+		req := httptest.NewRequest(http.MethodPost, "/", body)
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		e := echo.New()
+		config.EchoValidator(e)
+		c := e.NewContext(req, rec)
+
+		// mocks
+		mockAuth := mocks.AuthMock{}
+		tokenMock := mocks.TokenMock{}
+
+		// controller
+		controller := AuthController{AuthRepository: &mockAuth, TokensRepository: &tokenMock}
+
+		// test
+		err := controller.Login(c)
+		if assert.Error(t, err) {
+			assert.Equal(t, http.StatusBadRequest, err.(*echo.HTTPError).Code)
+		}
+
+	})
+
+	t.Run("Not found user", func(t *testing.T) {
+		// context
+		username := "antonio"
+		body := new(bytes.Buffer)
+		json.NewEncoder(body).Encode(map[string]string{"username": username, "password": "antonio1"})
+		req := httptest.NewRequest(http.MethodPost, "/", body)
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		e := echo.New()
+		config.EchoValidator(e)
+		c := e.NewContext(req, rec)
+
+		// mocks
+		mockAuth := mocks.AuthMock{}
+		mockAuth.On("UserForLogin", username).Return(&models.User{}, errors.New("error"))
+		tokenMock := mocks.TokenMock{}
+
+		// controller
+		controller := AuthController{AuthRepository: &mockAuth, TokensRepository: &tokenMock}
+
+		// test
+		err := controller.Login(c)
+		if assert.Error(t, err) {
+			assert.Equal(t, http.StatusUnauthorized, err.(*echo.HTTPError).Code)
+		}
+
+	})
+
+	t.Run("Find invalid password", func(t *testing.T) {
+		// context
+		user := &models.User{ID: "1", UserName: "jose", Password: "12345678"}
+		body := new(bytes.Buffer)
+		json.NewEncoder(body).Encode(map[string]string{"username": "jose", "password": "antonio1"})
+		req := httptest.NewRequest(http.MethodPost, "/", body)
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		e := echo.New()
+		config.EchoValidator(e)
+		c := e.NewContext(req, rec)
+
+		// mocks
+		mockAuth := mocks.AuthMock{}
+		mockAuth.On("UserForLogin", user.UserName).Return(user, nil)
+		tokenMock := mocks.TokenMock{}
+
+		// controller
+		controller := AuthController{AuthRepository: &mockAuth, TokensRepository: &tokenMock}
+
+		// test
+		err := controller.Login(c)
+		if assert.Error(t, err) {
+			assert.Equal(t, http.StatusUnauthorized, err.(*echo.HTTPError).Code)
+		}
+
+	})
+
+	t.Run("Not generate tokens", func(t *testing.T) {
+		// context
+		user := &models.User{ID: "1", UserName: "jose", Password: "12345678"}
 		body := new(bytes.Buffer)
 		json.NewEncoder(body).Encode(map[string]string{"username": "jose", "password": "12345678"})
 		req := httptest.NewRequest(http.MethodPost, "/", body)
@@ -85,17 +164,76 @@ func TestLogin(t *testing.T) {
 
 		// mocks
 		mockAuth := mocks.AuthMock{}
-		mockAuth.On("UserForLogin", authorizedUser.UserName).Return(authorizedUser, nil)
-		mockAuth.On("RegisterRefresh", authorizedUser.ID, "").Return(nil)
-
+		mockAuth.On("UserForLogin", user.UserName).Return(user, nil)
 		tokenMock := mocks.TokenMock{}
-		tokenMock.On("GenerateTokens", authorizedUser.ID).Return(nil)
+		tokenMock.On("GenerateTokens", user.ID).Return(errors.New("error"))
 
 		// controller
 		controller := AuthController{AuthRepository: &mockAuth, TokensRepository: &tokenMock}
 
 		// test
-		if assert.NoError(t, controller.Login(c)) {
+		err := controller.Login(c)
+		if assert.Error(t, err) {
+			assert.Equal(t, http.StatusInternalServerError, err.(*echo.HTTPError).Code)
+		}
+
+	})
+
+	t.Run("Not save refresh token", func(t *testing.T) {
+		// context
+		user := &models.User{ID: "1", UserName: "jose", Password: "12345678"}
+		body := new(bytes.Buffer)
+		json.NewEncoder(body).Encode(map[string]string{"username": "jose", "password": "12345678"})
+		req := httptest.NewRequest(http.MethodPost, "/", body)
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		e := echo.New()
+		config.EchoValidator(e)
+		c := e.NewContext(req, rec)
+
+		// mocks
+		mockAuth := mocks.AuthMock{}
+		mockAuth.On("UserForLogin", user.UserName).Return(user, nil)
+		mockAuth.On("RegisterRefresh", user.ID, "").Return(errors.New("error"))
+		tokenMock := mocks.TokenMock{}
+		tokenMock.On("GenerateTokens", user.ID).Return(nil)
+
+		// controller
+		controller := AuthController{AuthRepository: &mockAuth, TokensRepository: &tokenMock}
+
+		// test
+		err := controller.Login(c)
+		if assert.Error(t, err) {
+			assert.Equal(t, http.StatusInternalServerError, err.(*echo.HTTPError).Code)
+		}
+
+	})
+
+	t.Run("Login successfully", func(t *testing.T) {
+		// context
+		user := &models.User{ID: "1", UserName: "jose", Password: "12345678"}
+		body := new(bytes.Buffer)
+		json.NewEncoder(body).Encode(map[string]string{"username": "jose", "password": "12345678"})
+		req := httptest.NewRequest(http.MethodPost, "/", body)
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		e := echo.New()
+		config.EchoValidator(e)
+		c := e.NewContext(req, rec)
+
+		// mocks
+		mockAuth := mocks.AuthMock{}
+		mockAuth.On("UserForLogin", user.UserName).Return(user, nil)
+		mockAuth.On("RegisterRefresh", user.ID, "").Return(nil)
+		tokenMock := mocks.TokenMock{}
+		tokenMock.On("GenerateTokens", user.ID).Return(nil)
+
+		// controller
+		controller := AuthController{AuthRepository: &mockAuth, TokensRepository: &tokenMock}
+
+		// test
+		err := controller.Login(c)
+		if assert.NoError(t, err) {
 			assert.Equal(t, http.StatusOK, rec.Code)
 		}
 
@@ -104,54 +242,168 @@ func TestLogin(t *testing.T) {
 }
 
 func TestRefresh(t *testing.T) {
-	testCases := []struct {
-		Name   string
-		Cookie string
-		Code   int
-	}{
-		{Name: "Without Cookie", Code: http.StatusUnauthorized},
-		{Name: "Fail valid refresh", Cookie: "2", Code: http.StatusUnauthorized},
-		{Name: "Successful refresh", Cookie: "1", Code: http.StatusOK},
-	}
+	t.Run("Fail missing cookie", func(t *testing.T) {
+		// context
+		req := httptest.NewRequest(http.MethodPost, "/", nil)
+		rec := httptest.NewRecorder()
+		e := echo.New()
+		c := e.NewContext(req, rec)
 
-	validCookie := "1"
-	invalidCookie := "2"
-	authorizedUser := &models.User{ID: "1", UserName: "jose", Password: "12345678"}
-	session := &models.Session{UserID: "1", ID: 1}
+		// mocks
+		mockAuth := mocks.AuthMock{}
+		tokenMock := mocks.TokenMock{}
 
-	for i := range testCases {
-		testCase := testCases[i]
+		// controller
+		controller := AuthController{AuthRepository: &mockAuth, TokensRepository: &tokenMock}
 
-		t.Run(testCase.Name, func(t *testing.T) {
-			t.Parallel()
+		// test
+		err := controller.Refresh(c)
+		if assert.Error(t, err) {
+			assert.Equal(t, http.StatusUnauthorized, err.(*echo.HTTPError).Code)
+		}
 
-			req := httptest.NewRequest(http.MethodPost, "/", nil)
-			if len(testCase.Cookie) > 0 {
-				req.Header.Set(echo.HeaderCookie, fmt.Sprintf(`%v="%v"`, utils.RefreshCookieName, testCase.Cookie))
-			}
-			rec := httptest.NewRecorder()
-			e := echo.New()
-			c := e.NewContext(req, rec)
+	})
 
-			// mocks
-			mockAuth := mocks.AuthMock{}
-			mockAuth.On("ValidRefresh", validCookie).Return(session, nil)
-			mockAuth.On("ValidRefresh", invalidCookie).Return(session, errors.New("error"))
-			mockAuth.On("RegisterRefresh", authorizedUser.ID, authorizedUser.ID).Return(nil)
-			mockAuth.On("DeleteRefresh", session.ID).Return(nil)
-			tokenMock := mocks.TokenMock{}
-			tokenMock.On("GenerateTokens", authorizedUser.ID).Return(authorizedUser.ID)
+	t.Run("Not found refresh token", func(t *testing.T) {
+		// context
+		cookie := "1"
+		req := httptest.NewRequest(http.MethodPost, "/", nil)
+		req.Header.Set(echo.HeaderCookie, fmt.Sprintf(`%v="%v"`, utils.RefreshCookieName, cookie))
+		rec := httptest.NewRecorder()
+		e := echo.New()
+		c := e.NewContext(req, rec)
 
-			// controller
-			controller := AuthController{AuthRepository: &mockAuth, TokensRepository: &tokenMock}
+		// mocks
+		mockAuth := mocks.AuthMock{}
+		mockAuth.On("ValidRefresh", cookie).Return(&models.Session{}, errors.New("error"))
+		tokenMock := mocks.TokenMock{}
 
-			// test
-			if assert.NoError(t, controller.Refresh(c)) {
-				assert.Equal(t, testCase.Code, rec.Code)
-			}
+		// controller
+		controller := AuthController{AuthRepository: &mockAuth, TokensRepository: &tokenMock}
 
-		})
-	}
+		// test
+		err := controller.Refresh(c)
+		if assert.Error(t, err) {
+			assert.Equal(t, http.StatusUnauthorized, err.(*echo.HTTPError).Code)
+		}
+
+	})
+
+	t.Run("Not generate tokens", func(t *testing.T) {
+		// context
+		cookie := "1"
+		session := &models.Session{UserID: "1", ID: 1}
+		req := httptest.NewRequest(http.MethodPost, "/", nil)
+		req.Header.Set(echo.HeaderCookie, fmt.Sprintf(`%v="%v"`, utils.RefreshCookieName, cookie))
+		rec := httptest.NewRecorder()
+		e := echo.New()
+		c := e.NewContext(req, rec)
+
+		// mocks
+		mockAuth := mocks.AuthMock{}
+		mockAuth.On("ValidRefresh", cookie).Return(session, nil)
+		tokenMock := mocks.TokenMock{}
+		tokenMock.On("GenerateTokens", session.UserID).Return(errors.New("error"))
+
+		// controller
+		controller := AuthController{AuthRepository: &mockAuth, TokensRepository: &tokenMock}
+
+		// test
+		err := controller.Refresh(c)
+		if assert.Error(t, err) {
+			assert.Equal(t, http.StatusInternalServerError, err.(*echo.HTTPError).Code)
+		}
+
+	})
+
+	t.Run("Not save refresh token", func(t *testing.T) {
+		// context
+		cookie := "1"
+		session := &models.Session{UserID: "1", ID: 1}
+		req := httptest.NewRequest(http.MethodPost, "/", nil)
+		req.Header.Set(echo.HeaderCookie, fmt.Sprintf(`%v="%v"`, utils.RefreshCookieName, cookie))
+		rec := httptest.NewRecorder()
+		e := echo.New()
+		c := e.NewContext(req, rec)
+
+		// mocks
+		mockAuth := mocks.AuthMock{}
+		mockAuth.On("ValidRefresh", cookie).Return(session, nil)
+		mockAuth.On("RegisterRefresh", session.UserID, "").Return(errors.New("error"))
+		tokenMock := mocks.TokenMock{}
+		tokenMock.On("GenerateTokens", session.UserID).Return(nil)
+
+		// controller
+		controller := AuthController{AuthRepository: &mockAuth, TokensRepository: &tokenMock}
+
+		// test
+		err := controller.Refresh(c)
+		if assert.Error(t, err) {
+			assert.Equal(t, http.StatusInternalServerError, err.(*echo.HTTPError).Code)
+		}
+
+	})
+
+	t.Run("Not delete refresh token", func(t *testing.T) {
+		t.Parallel()
+
+		cookie := "1"
+		session := &models.Session{UserID: "1", ID: 1}
+
+		// context
+		req := httptest.NewRequest(http.MethodPost, "/", nil)
+		req.Header.Set(echo.HeaderCookie, fmt.Sprintf(`%v="%v"`, utils.RefreshCookieName, cookie))
+		rec := httptest.NewRecorder()
+		e := echo.New()
+		c := e.NewContext(req, rec)
+
+		// mocks
+		mockAuth := mocks.AuthMock{}
+		mockAuth.On("ValidRefresh", cookie).Return(session, nil)
+		mockAuth.On("DeleteRefresh", session.ID).Return(errors.New("error"))
+		mockAuth.On("RegisterRefresh", session.UserID, "").Return(nil)
+		tokenMock := mocks.TokenMock{}
+		tokenMock.On("GenerateTokens", session.UserID).Return(nil)
+
+		// controller
+		controller := AuthController{AuthRepository: &mockAuth, TokensRepository: &tokenMock}
+
+		// test
+		err := controller.Refresh(c)
+		if assert.Error(t, err) {
+			assert.Equal(t, http.StatusInternalServerError, err.(*echo.HTTPError).Code)
+		}
+
+	})
+
+	t.Run("Successfully refresh", func(t *testing.T) {
+		cookie := "1"
+		session := &models.Session{UserID: "1", ID: 1}
+		// context
+		req := httptest.NewRequest(http.MethodPost, "/", nil)
+		req.Header.Set(echo.HeaderCookie, fmt.Sprintf(`%v="%v"`, utils.RefreshCookieName, "1"))
+		rec := httptest.NewRecorder()
+		e := echo.New()
+		c := e.NewContext(req, rec)
+
+		// mocks
+		mockAuth := mocks.AuthMock{}
+		mockAuth.On("ValidRefresh", cookie).Return(session, nil)
+		mockAuth.On("RegisterRefresh", session.UserID, "").Return(nil)
+		mockAuth.On("DeleteRefresh", session.ID).Return(nil)
+		tokenMock := mocks.TokenMock{}
+		tokenMock.On("GenerateTokens", session.UserID).Return(nil)
+
+		// controller
+		controller := AuthController{AuthRepository: &mockAuth, TokensRepository: &tokenMock}
+
+		// test
+		err := controller.Refresh(c)
+		if assert.NoError(t, err) {
+			assert.Equal(t, http.StatusOK, rec.Code)
+		}
+
+	})
 
 }
 
@@ -160,12 +412,10 @@ func TestLogout(t *testing.T) {
 		Name   string
 		Cookie string
 		Code   int
-		Delete bool
 	}{
 		{Name: "Without Cookie", Code: http.StatusUnauthorized},
 		{Name: "Invalid cookie", Cookie: "2", Code: http.StatusUnauthorized},
-		{Name: "Successful logout", Cookie: "1", Code: http.StatusOK},
-		{Name: "Error deleting refresh", Cookie: "1", Code: http.StatusInternalServerError, Delete: true},
+		{Name: "Error deleting refresh", Cookie: "1", Code: http.StatusInternalServerError},
 	}
 
 	// Define a valid session
@@ -190,22 +440,42 @@ func TestLogout(t *testing.T) {
 			mockAuth := mocks.AuthMock{}
 			mockAuth.On("ValidRefresh", validCookie).Return(session, nil)
 			mockAuth.On("ValidRefresh", invalidCookie).Return(session, errors.New("error"))
-			//mockAuth.On("DeleteRefresh", session.ID).Return(nil)
-			//mockAuth.On("DeleteRefresh", session.ID).Return(errors.New("error"))
-
-			if testCase.Delete {
-				mockAuth.On("DeleteRefresh", session.ID).Return(errors.New("error"))
-			} else {
-				mockAuth.On("DeleteRefresh", session.ID).Return(nil)
-			}
+			mockAuth.On("DeleteRefresh", session.ID).Return(errors.New("error"))
+			mockAuth.On("DeleteRefresh", session.ID).Return(nil)
 
 			// Create the controller with the mock
 			controller := AuthController{AuthRepository: &mockAuth}
 
 			// Run the test
-			if assert.NoError(t, controller.Logout(c)) {
-				assert.Equal(t, testCase.Code, rec.Code)
+			err := controller.Logout(c)
+			if assert.Error(t, err) {
+				assert.Equal(t, testCase.Code, err.(*echo.HTTPError).Code)
 			}
 		})
+
 	}
+
+	t.Run("Logout Successfully ", func(t *testing.T) {
+		// Create a new HTTP request
+		cookie := "1"
+		session := &models.Session{UserID: "1", ID: 1}
+		req := httptest.NewRequest(http.MethodPost, "/", nil)
+		req.Header.Set(echo.HeaderCookie, fmt.Sprintf(`%v="%v"`, utils.RefreshCookieName, cookie))
+		rec := httptest.NewRecorder()
+		e := echo.New()
+		c := e.NewContext(req, rec)
+
+		// Define the mock behavior
+		mockAuth := mocks.AuthMock{}
+		mockAuth.On("ValidRefresh", cookie).Return(session, nil)
+		mockAuth.On("DeleteRefresh", session.ID).Return(nil)
+
+		// Create the controller with the mock
+		controller := AuthController{AuthRepository: &mockAuth}
+
+		// Run the test
+		if assert.NoError(t, controller.Logout(c)) {
+			assert.Equal(t, http.StatusOK, rec.Code)
+		}
+	})
 }
