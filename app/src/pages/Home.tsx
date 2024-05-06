f3b98991d1a28a6b74616628d8ec0e98a8c82b78
chris-sanchez098 <christian.valenzuela@correounivalle.edu.co>
2024-05-06 16:44:51 -0500

    Feat: add confirmation modal

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,14 +1,6 @@
-import LogoutButton from '~/components/LogoutButton';
-
 function Home() {
 	return (
 		<div class='flex flex-col h-full justify-between'>
-			<nav class='p-6'>
-				<div class='flex items-center justify-between'>
-					<div class='text-white text-2xl' />
-					<LogoutButton navigateTo='/login' />
-				</div>
-			</nav>
 			<div class='mb-auto ...'>
 				<div class='container mx-auto p-4'>
 					<h1 class='text-4xl font-bold text-center'>Home</h1>
--- app/src/utils/constans.ts
+++ app/src/utils/constans.ts
@@ -1,1 +1,19 @@
 export const QUERY_LIMIT = 20;
+
+export const MESSAGES = {
+	logout: {
+		title: 'Cerrar sesión',
+		description: '¿Estás seguro que deseas cerrar sesión?',
+		confirm: 'Sessión cerrada correctamente',
+		error: 'Error al cerrar sesión',
+	},
+	session: {
+		expired: 'Tu sesión ha expirado, por favor vuelve a iniciar sesión',
+		successful: 'Sesión iniciada correctamente',
+		error: 'Error al iniciar sesión, por favor intenta de nuevo',
+	},
+	auth: {
+		error: 'Revisa tu usuario y contraseña',
+		password: 'La contraseña debe tener al menos 8 caracteres',
+	},
+};
