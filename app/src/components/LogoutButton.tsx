519e4d7f18775a101d5bcc06597fc2a24bde4cc4
chris-sanchez098 <christian.valenzuela@correounivalle.edu.co>
2024-05-06 16:45:38 -0500

    Style: style logoutButton

--- app/src/components/LogoutButton.tsx
+++ app/src/components/LogoutButton.tsx
@@ -1,25 +1,52 @@
 import { useNavigate } from '@solidjs/router';
+import { RiSystemLogoutBoxLine } from 'solid-icons/ri';
 import { createSignal } from 'solid-js';
-import { Button } from '~/components/ui/Button';
+import toast from 'solid-toast';
 import { logoutRequest } from '~/modules/auth/requests/authRequests';
+import { MESSAGES } from '~/utils/constans';
+import ConfirmationModal from './ConfirmationModal';
 
 const LogoutButton = (props: { navigateTo: string }) => {
 	const navigate = useNavigate();
 	const [navigateTo] = createSignal(props.navigateTo);
+	const [showModal, setShowModal] = createSignal(false);
 
 	const logout = async () => {
 		try {
 			await logoutRequest();
-			navigate(`${navigateTo()}?reason=LOG_OUT`, { replace: true });
+			navigate(`${navigateTo()}`, { replace: true });
+			toast.success(MESSAGES.logout.confirm);
 		} catch (error) {
-			console.error(error);
+			toast.error(MESSAGES.logout.error)
 		}
 	};
 
+	const handleLogout = () => {
+		setShowModal(true);
+	};
+
 	return (
-		<Button onClick={logout} class='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded'>
-			Logout
-		</Button>
+		<>
+			<button
+				type='button'
+				onclick={handleLogout}
+				class='w-full flex items-center group  p-2 font-semibold text-sm rounded-lg text-white hover:bg-practice_date dark:hover:bg-gray-700 gap-1 p transition-colors duration-200'
+			>
+				<RiSystemLogoutBoxLine size={24} />
+				<span class='ml-3'>Cerrar sesión</span>
+				<div class='ml-auto w-1 rounded-xl h-8 bg-transparent transition-colors duration-200 relative overflow-hidden'>
+					<span class='absolute top-0 left-0 w-full h-[102%] translate-y-full group-hover:translate-y-0 bg-white transition-all duration-300' />
+				</div>
+			</button>
+			{showModal() && (
+				<ConfirmationModal
+					title={MESSAGES.logout.title}
+					message={MESSAGES.logout.description}
+					onConfirm={logout}
+					onCancel={() => setShowModal(false)}
+				/>
+			)}
+		</>
 	);
 };
 
