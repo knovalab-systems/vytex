797afd9c820e8e57354ba5efc63aa3b7622cd813
andresNeveu <vordones64@gmail.com>
2024-05-10 17:06:15 -0500

    Chore: Add tanstack query

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,15 +3,19 @@ import './App.css';
 import { Router } from '@solidjs/router';
 import { Toaster } from 'solid-toast';
 import Routes from './routes/Routes';
+import { QueryClientProvider } from '@tanstack/solid-query';
+import { queryClient } from './utils/queryClient';
 
 function App() {
 	return (
-		<Suspense>
-			<Router>
-				<Routes />
-			</Router>
-			<Toaster />
-		</Suspense>
+		<QueryClientProvider client={queryClient}>
+			<Suspense>
+				<Router>
+					<Routes />
+				</Router>
+				<Toaster />
+			</Suspense>
+		</QueryClientProvider>
 	);
 }
 
