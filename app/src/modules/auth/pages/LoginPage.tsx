0c1b3f1fad0c32e889f18e1dff9dd5294f5e9be3
andresNeveu <vordones64@gmail.com>
2024-05-10 17:25:21 -0500

    Fix: avoid refetch on login

--- app/src/modules/auth/pages/LoginPage.tsx
+++ app/src/modules/auth/pages/LoginPage.tsx
@@ -1,16 +1,17 @@
 import { Navigate, useSearchParams } from '@solidjs/router';
-import { createResource, Switch, Match } from 'solid-js';
+import { createQuery } from '@tanstack/solid-query';
+import { Match, Switch } from 'solid-js';
 import LoginForm from '../components/LoginForm';
 import { refreshRequest } from '../requests/authRequests';
 
 function LoginPage() {
 	const [searchParams, _] = useSearchParams();
-	const [token] = createResource(searchParams.reason ? 0 : 1, refreshRequest);
+	const refresh = createQuery(() => refreshRequest(!searchParams.reason));
 
 	return (
 		<Switch>
-			<Match when={token.state === 'ready'}>{<Navigate href={'/'} />}</Match>
-			<Match when={token.state === 'errored'}>{<LoginForm />}</Match>
+			<Match when={refresh.isSuccess}>{<Navigate href={'/'} />}</Match>
+			<Match when={refresh.isError || !!searchParams}>{<LoginForm />}</Match>
 		</Switch>
 	);
 }
