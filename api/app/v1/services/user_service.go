17cdc62ece505ca58ce07bd11ee7ce973480ab08
andresNeveu <vordones64@gmail.com>
2024-05-16 00:17:24 -0500

    Feat:  delete_at at user update

--- api/app/v1/services/user_service.go
+++ api/app/v1/services/user_service.go
@@ -48,7 +48,7 @@ func (m *UserService) UpdateUser(update *models.UpdateUserBody) (*models.User, e
 		return nil, problems.UpdateUsersBadRequest()
 	}
 
-	rows, err := table.Where(table.ID.Eq(update.ID)).Updates(updateMap)
+	rows, err := table.Unscoped().Where(table.ID.Eq(update.ID)).Updates(updateMap)
 	if err != nil {
 		return nil, problems.ServerError()
 	}
@@ -57,7 +57,7 @@ func (m *UserService) UpdateUser(update *models.UpdateUserBody) (*models.User, e
 		return nil, problems.UpdateUsersBadRequest()
 	}
 
-	user, err := table.Where(table.ID.Eq(update.ID)).First()
+	user, err := table.Unscoped().Where(table.ID.Eq(update.ID)).First()
 	if err != nil {
 		return nil, problems.ServerError()
 	}
