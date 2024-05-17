01dcd2fc21f9c357398126764c1e9a5425240221
Víctor Andrés Sapuyes <54079546+andresNeveu@users.noreply.github.com>
2024-05-17 14:25:26 -0500

    Update query_model.go

--- api/app/v1/models/query_model.go
+++ api/app/v1/models/query_model.go
@@ -6,7 +6,7 @@ type Query struct {
 	Limit  *int   `query:"limit" validate:"omitnil,gte=-1"`
 	Offset int    `query:"offset" validate:"gte=0"`
 	Page   int    `query:"page" validate:"gte=0"`
-	Filter string `query:"filter" validate:"omitempty"`
+	Filter string `query:"filter"`
 }
 
 type UserFilter struct {
