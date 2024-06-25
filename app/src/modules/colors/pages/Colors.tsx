80192d7a87e39304fdda4395554d43734293b2bc
andresNeveu <vordones64@gmail.com>
2024-06-25 12:14:38 -0500

    Feat: get colors request

--- app/src/modules/colors/pages/Colors.tsx
+++ app/src/modules/colors/pages/Colors.tsx
@@ -1,7 +1,20 @@
+import { createQuery } from '@tanstack/solid-query';
+import { getColorsQuery } from '../requests/colorsGetRequests';
+import { Match, Switch } from 'solid-js';
+import Loading from '~/components/Loading';
+
 function Colors() {
+	const colors = createQuery(() => getColorsQuery(0));
 	return (
-		<div>
-			<h2>Colors</h2>
+		<div class='h-full flex flex-col'>
+			<Switch>
+				<Match when={colors.isLoading}>
+					<Loading label='Cargando colores' />
+				</Match>
+				<Match when={colors.isSuccess}>
+					<h2>Colors</h2>
+				</Match>
+			</Switch>
 		</div>
 	);
 }
