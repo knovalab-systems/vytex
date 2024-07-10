14d5de2bb16ce075c4457e962671c72ce98f99b4
andresNeveu <vordones64@gmail.com>
2024-07-10 15:59:06 -0500

    Feat: fabric n resource fields

--- app/src/modules/resource/requests/resourcesGetRequests.ts
+++ app/src/modules/resource/requests/resourcesGetRequests.ts
@@ -15,6 +15,7 @@ async function getResources(page: number) {
 		readResources({
 			page: page,
 			limit: QUERY_LIMIT,
+			fields: ['id', 'key', 'name', 'cost', 'color_id', 'code', 'deleted_at'],
 		}),
 	);
 }
