35d64a7b303f7ddd8756374cdf54a1454ddb2d75
andresNeveu <vordones64@gmail.com>
2024-07-12 15:56:54 -0500

    Feat(app): supplier routes, table, test

---
+++ app/src/modules/supplier/requests/supplierGet.ts
@@ -0,0 +1,40 @@
+import { queryOptions } from '@tanstack/solid-query';
+import { aggregate, readSuppliers } from '@vytex/client';
+import { QUERY_LIMIT } from '~/constants/http';
+import { client } from '~/lib/client';
+
+export function getSuppliersQuery(page: number) {
+	return queryOptions({
+		queryKey: ['getSuppliers', page],
+		queryFn: () => getSuppliers(page),
+	});
+}
+
+async function getSuppliers(page: number) {
+	return await client.request(
+		readSuppliers({
+			page: page,
+			limit: QUERY_LIMIT,
+			fields: ['id', 'nit', 'name', 'deleted_at'],
+		}),
+	);
+}
+
+export type GetSuppliersType = Awaited<ReturnType<typeof getSuppliers>>;
+
+export function countSuppliersQuery() {
+	return queryOptions({
+		queryKey: ['countSuppliers'],
+		queryFn: () => countSuppliers(),
+	});
+}
+
+async function countSuppliers() {
+	return await client.request(
+		aggregate('vytex_suppliers', {
+			aggregate: {
+				count: '*',
+			},
+		}),
+	);
+}
