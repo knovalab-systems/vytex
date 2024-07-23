6cc790c6916d4790ced7a98140acd0e34070dc53
chris-sanchez098 <christian.valenzuela@correounivalle.edu.co>
2024-07-22 19:26:23 -0500

    feat(app): add get references request n routes

---
+++ app/src/modules/reference/pages/References.tsx
@@ -0,0 +1,63 @@
+import { A } from '@solidjs/router';
+import { createQuery } from '@tanstack/solid-query';
+import { AiOutlinePlus } from 'solid-icons/ai';
+import { Match, Switch, createMemo, createSignal } from 'solid-js';
+import Loading from '~/components/Loading';
+import { Button } from '~/components/ui/Button';
+import {
+    Pagination,
+    PaginationEllipsis,
+    PaginationItem,
+    PaginationItems,
+    PaginationNext,
+    PaginationPrevious,
+} from '~/components/ui/Pagination';
+import { QUERY_LIMIT } from '~/constants/http';
+import { REFS_CREATE_PATH } from '~/constants/paths';
+import ReferenceTable from '../components/ReferenceTable';
+import { countReferencesQuery, getReferencesQuery } from '../requests/referenceGet';
+
+function References() {
+    const [page, setPage] = createSignal(1);
+    const references = createQuery(() => getReferencesQuery(page()));
+    const countReferences = createQuery(() => countReferencesQuery());
+    const pages = createMemo<number>(() => {
+        const count = countReferences.data?.at(0)?.count || 1;
+        const safe = count === 0 ? 1 : count;
+        return Math.ceil(safe / QUERY_LIMIT);
+    });
+
+    return (
+        <div class='h-full w-full flex flex-col'>
+            <div class='mb-2'>
+                <A href={REFS_CREATE_PATH}>
+                    <Button class='bg-purple-600 hover:bg-purple-700'>
+                        Nueva Referencia <AiOutlinePlus class='ml-2' size={22} />
+                    </Button>
+                </A>
+            </div>
+            <Switch>
+                <Match when={references.isLoading || countReferences.isLoading}>
+                    <Loading label='Cargando referencias' />
+                </Match>
+                <Match when={references.isSuccess && countReferences.isSuccess}>
+                    <ReferenceTable references={references.data} />
+                    <Pagination
+                        class='pt-2 [&>*]:justify-center'
+                        count={pages()}
+                        page={page()}
+                        onPageChange={setPage}
+                        itemComponent={props => <PaginationItem page={props.page}>{props.page}</PaginationItem>}
+                        ellipsisComponent={() => <PaginationEllipsis />}
+                    >
+                        <PaginationPrevious />
+                        <PaginationItems />
+                        <PaginationNext />
+                    </Pagination>
+                </Match>
+            </Switch>
+        </div>
+    );
+}
+
+export default References;
---
+++ app/src/modules/reference/requests/referenceGet.ts
@@ -0,0 +1,40 @@
+import { queryOptions } from '@tanstack/solid-query';
+import { aggregate, readReference } from '@vytex/client';
+import { QUERY_LIMIT } from '~/constants/http';
+import { client } from '~/lib/client';
+
+export function getReferencesQuery(page: number) {
+	return queryOptions({
+		queryKey: ['getReference', page],
+		queryFn: () => getReferences(page),
+	});
+}
+
+async function getReferences(page: number) {
+	return await client.request(
+		readReference({
+			page: page,
+			limit: QUERY_LIMIT,
+			fields: ['id', 'code', 'created_at', 'deleted_at'],
+		}),
+	);
+}
+
+export function countReferencesQuery() {
+	return queryOptions({
+		queryKey: ['countReference'],
+		queryFn: () => countReferences(),
+	});
+}
+
+async function countReferences() {
+	return await client.request(
+		aggregate('vytex_references', {
+			aggregate: {
+				count: '*',
+			},
+		}),
+	);
+}
+
+export type GetReferenceType = Awaited<ReturnType<typeof getReferences>>;
