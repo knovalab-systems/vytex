4126e062c24062cb88c013b16b3b3bde4f4164a3
chris-sanchez098 <christian.valenzuela@correounivalle.edu.co>
2024-07-23 13:26:34 -0500

    feat(app): add supplier update request n add form submmit handler

--- app/src/modules/supplier/components/SupplierUpdateForm.tsx
+++ app/src/modules/supplier/components/SupplierUpdateForm.tsx
@@ -1,10 +1,17 @@
-import { createForm, valiForm } from "@modular-forms/solid";
+import { getLocalTimeZone, now } from '@internationalized/date';
+import { type SubmitHandler, createForm, setValue, valiForm } from "@modular-forms/solid";
 import { useNavigate } from "@solidjs/router";
+import toast from "solid-toast";
 import { Button } from "~/components/ui/Button";
 import { Input } from "~/components/ui/Input";
-import { Label } from "~/components/ui/Label";
+import { Label, LabelSpan } from "~/components/ui/Label";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "~/components/ui/Select";
+import { STATUS_CODE } from "~/constants/http";
 import { SUPPLIERS_PATH } from "~/constants/paths";
+import { STATUS_OPTIONS } from "~/constants/status";
+import type { Supplier } from "~/schemas/core";
 import type { GetSupplierType } from "../requests/supplierGet";
+import { updateSupplierRequest } from "../requests/supplierUpdate";
 import { SupplierUpdateSchema, type SupplierUpdateType } from "../schemas/supplierUpdate";
 
 function SupplireUpdateForm(props: { supplier?: GetSupplierType }) {
@@ -13,19 +20,65 @@ function SupplireUpdateForm(props: { supplier?: GetSupplierType }) {
     const [form, { Form, Field }] = createForm<SupplierUpdateType>({
         validate: valiForm(SupplierUpdateSchema),
         initialValues: {
-            name: props.supplier?.name || 'pepe',
+            name: props.supplier?.name,
             brand: props.supplier?.brand,
-            code: props.supplier?.code,
-            nit: props.supplier?.nit,
+            code: Number(props.supplier?.code),
+            nit: Number(props.supplier?.nit),
+            deleted_at: !props.supplier?.deleted_at ? 'Activo' : 'Inactivo',
         }
     });
 
-    const handleCancel = () => navigate(SUPPLIERS_PATH);
 
-    const handleSubmit = async (data: SupplierUpdateType) => {
-        console.log(data);
+    const handleSubmit: SubmitHandler<SupplierUpdateType> = async data => {
+        const newData = {
+            ...data,
+            code: String(data.code),
+            nit: String(data.nit)
+        };
+
+        const { deleted_at, ...rest } = newData;
+        const supplier: Supplier = Object.keys(rest).reduce((p: Supplier, v) => {
+            const field = rest[v as keyof typeof rest];
+            const oldField = props.supplier?.[v];
+            if (field && field !== oldField) {
+                p[v as keyof typeof p] = field;
+            }
+            return p;
+        }, {});
+
+        if (!STATUS_OPTIONS[deleted_at as keyof typeof STATUS_OPTIONS] && !props.supplier?.deleted_at) {
+            supplier.deleted_at = now(getLocalTimeZone()).toAbsoluteString();
+        } else if (STATUS_OPTIONS[deleted_at as keyof typeof STATUS_OPTIONS] && Boolean(props.supplier?.deleted_at)) {
+            supplier.deleted_at = null;
+        }
+
+        if (Object.keys(supplier).length === 0) return;
+
+        return updateSupplierRequest(props.supplier?.id, supplier)
+            .then(() => {
+                toast.success('Proveedor actualizado correctamente');
+                navigate(SUPPLIERS_PATH);
+            })
+            .catch(error => {
+                if (error.response.status === STATUS_CODE.conflict) {
+                    switch (error.errors.detail) {
+                        case 'Supplier code already exists':
+                            toast.error(`El código "${data.code}" no está disponible. Intente con otro.`);
+                            break;
+                        case 'Supplier NIT already exists':
+                            toast.error(`El NIT "${data.nit}" no está disponible. Intente con otro.`);
+                            break;
+                        default:
+                            toast.error(`El NIT "${data.nit}" y el código "${data.code}" no están disponibles. Intente con otros.`);
+                            break;
+                    }
+                } else {
+                    toast.error('Error al actualizar proveedor.');
+                }
+            });
     };
 
+    const handleCancel = () => navigate(SUPPLIERS_PATH);
 
     return (
         <Form class='w-full lg:w-2/5 2xl:w-1/4' onSubmit={handleSubmit}>
@@ -94,6 +147,27 @@ function SupplireUpdateForm(props: { supplier?: GetSupplierType }) {
                                 value={field.value}
                                 {...props}
                             />
+                        </div>
+                    )}
+                </Field>
+                <Field name='deleted_at'>
+                    {field => (
+                        <div>
+                            <LabelSpan>Estado</LabelSpan>
+                            <Select<string>
+                                value={field.value}
+                                onChange={value => {
+                                    setValue(form, 'deleted_at', value);
+                                }}
+                                options={Object.keys(STATUS_OPTIONS)}
+                                placeholder='Selecciona un rol'
+                                itemComponent={props => <SelectItem item={props.item}>{props.item.rawValue}</SelectItem>}
+                            >
+                                <SelectTrigger aria-label='Roles'>
+                                    <SelectValue<string>>{state => state.selectedOption()}</SelectValue>
+                                </SelectTrigger>
+                                <SelectContent />
+                            </Select>
                         </div>
                     )}
                 </Field>
--- app/src/modules/supplier/schemas/supplierUpdate.ts
+++ app/src/modules/supplier/schemas/supplierUpdate.ts
@@ -1,14 +1,16 @@
-import { type InferInput, check, minLength, minValue, number, object, pipe, string } from 'valibot';
+import { type InferInput, check, minLength, minValue, number, object, picklist, pipe, string } from 'valibot';
+import { STATUS_OPTIONS } from '~/constants/status';
 
 export const SupplierUpdateSchema = object({
 	name: pipe(string(), minLength(1, 'Ingresa el nombre.')),
 	brand: pipe(string(), minLength(1, 'Ingresa la marca.')),
+	code: pipe(number(), minValue(1, 'Ingresa el código.')),
 	nit: pipe(
-		number('Ingresa el NIT.'),
+		number(),
 		minValue(1, 'Ingresa el NIT.'),
 		check(n => String(n).length === 9, 'El NIT debe ser de 9 dígitos'),
 	),
-	code: pipe(number('Ingresa el código.'), minValue(1, 'Ingresa el código.')),
+	deleted_at: picklist<string[]>(Object.keys(STATUS_OPTIONS)),
 });
 
 export type SupplierUpdateType = InferInput<typeof SupplierUpdateSchema>;
