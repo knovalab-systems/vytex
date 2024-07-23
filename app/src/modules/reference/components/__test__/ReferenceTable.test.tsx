7e901a3a73449f5997ae8dd343d5b13b0d73d874
chris-sanchez098 <christian.valenzuela@correounivalle.edu.co>
2024-07-22 20:41:29 -0500

    test(app): test reference table

---
+++ app/src/modules/reference/components/__test__/ReferenceTable.test.tsx
@@ -0,0 +1,37 @@
+import { render, screen } from '@solidjs/testing-library';
+import '@testing-library/jest-dom';
+import type { GetReferenceType } from '../../requests/referenceGet';
+import ReferenceTable from '../ReferenceTable';
+
+describe('Reference Table', () => {
+    beforeEach(() => {
+        vi.resetAllMocks();
+    });
+
+    it('renders correctly on empty references', () => {
+        render(() => <ReferenceTable references={undefined} />);
+        const tableHeader = screen.getByText('No se han encontraron referencias.');
+
+        expect(tableHeader).toBeInTheDocument();
+    });
+
+    it('renders correctly on references', () => {
+        const references: GetReferenceType = [
+            {
+                id: 123,
+                code: 'REF001',
+                deleted_at: null,
+                created_at: null,
+            },
+        ];
+
+        render(() => <ReferenceTable references={references} />);
+        const referenceId = screen.getByText('123');
+        const referenceCode = screen.getByText('REF001');
+        const referenceStatus = screen.getByText('Activo');
+
+        expect(referenceId).toBeInTheDocument();
+        expect(referenceCode).toBeInTheDocument();
+        expect(referenceStatus).toBeInTheDocument();
+    });
+});
