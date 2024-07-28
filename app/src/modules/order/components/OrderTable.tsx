713d7c2e1202ba5f222b8ecf14020850a39e10a5
chris-sanchez098 <christian.valenzuela@correounivalle.edu.co>
2024-07-27 19:32:40 -0500

    style: table style adjusted

--- app/src/modules/order/components/OrderTable.tsx
+++ app/src/modules/order/components/OrderTable.tsx
@@ -5,45 +5,43 @@ import type { GetOrdersType } from '../request/OrderGet';
 
 function OrderTable(props: { orders?: GetOrdersType }) {
     return (
-        <Show when={props.orders}>
-            <TableContainer>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableHeader>ID</TableHeader>
-                            <TableHead>ID pedido</TableHead>
-                            <TableHead>Estado</TableHead>
-                            <TableHead>Fecha de creación</TableHead>
-                            <TableHead>Fecha de cancelación</TableHead>
-                            <TableHead>Fecha de finalización</TableHead>
-                            <TableHead>Acciones</TableHead>
+        <TableContainer>
+            <Table class='md:table-fixed'>
+                <TableHeader class='sticky top-0 z-10'>
+                    <TableRow class=' bg-indigo-500 *:text-white hover:bg-indigo-500'>
+                        <TableHead>ID</TableHead>
+                        <TableHead>ID pedido</TableHead>
+                        <TableHead>Estado</TableHead>
+                        <TableHead>Fecha de creación</TableHead>
+                        <TableHead>Fecha de cancelación</TableHead>
+                        <TableHead>Fecha de finalización</TableHead>
+                        <TableHead>Acciones</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    <Show when={(props.orders?.length ?? 0) === 0}>
+                        <TableRow class='bg-white'>
+                            <TableCell colspan={7}>No se han encontrado ordenes.</TableCell>
                         </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        <Show when={(props.orders?.length ?? 0) === 0}>
-                            <TableRow class='bg-white'>
-                                <TableCell colspan={7}>No se han encontraron ordenes.</TableCell>
+                    </Show>
+                    <For each={props.orders}>
+                        {order => (
+                            <TableRow class='bg-white group'>
+                                <TableCell>{order.id}</TableCell>
+                                <TableCell>{order.custom_id}</TableCell>
+                                <TableCell>{order.status === 'Created' ? 'creado' : ''}</TableCell>
+                                <TableCell>{parseDateTimeHuman(order.created_at)}</TableCell>
+                                <TableCell>{parseDateTimeHuman(order.canceled_at)}</TableCell>
+                                <TableCell>{parseDateTimeHuman(order.finished_at)}</TableCell>
+                                <TableCell>
+                                    Acciones
+                                </TableCell>
                             </TableRow>
-                        </Show>
-                        <For each={props.orders}>
-                            {order => (
-                                <TableRow class='bg-white group'>
-                                    <TableCell>{order.id}</TableCell>
-                                    <TableCell>{order.custom_id}</TableCell>
-                                    <TableCell>{order.status}</TableCell>
-                                    <TableCell>{parseDateTimeHuman(order.created_at)}</TableCell>
-                                    <TableCell>{parseDateTimeHuman(order.canceled_at)}</TableCell>
-                                    <TableCell>{parseDateTimeHuman(order.finished_at)}</TableCell>
-                                    <TableCell>
-                                        Acciones
-                                    </TableCell>
-                                </TableRow>
-                            )}
-                        </For>
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </Show>
+                        )}
+                    </For>
+                </TableBody>
+            </Table>
+        </TableContainer>
     );
 }
 
