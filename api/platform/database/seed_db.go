02323f1a5ed936e15310694491a78dbf715f6a02
chris-sanchez098 <christian.valenzuela@correounivalle.edu.co>
2024-08-08 20:45:19 -0500

    test: add create fake data on supplier, color, resource, composition n fabrics

--- api/platform/database/seed_db.go
+++ api/platform/database/seed_db.go
@@ -8,6 +8,7 @@ import (
 	"gorm.io/gorm"
 	"log"
 	"math/rand"
+	"sync"
 	"time"
 )
 
@@ -19,12 +20,52 @@ func SeedDB(db *gorm.DB) {
 	}
 
 	insertUsers(db, roles)
-	insertColors(db)
-	generateSupplier(db)
+
+	var wg sync.WaitGroup
+	functions := []func(*gorm.DB){insertColors, generateSupplier, generateComposition}
+
+	for _, fn := range functions {
+		wg.Add(1)
+		go func(f func(*gorm.DB)) {
+			defer wg.Done()
+			f(db)
+		}(fn)
+	}
+
+	wg.Wait()
+	generateResource(db)
+	generateFabric(db)
+}
+
+func reduceStringLength(input string, length int) string {
+	if len(input) > length {
+		return input[:length]
+	}
+	return input
+}
+
+func generateColorCode() string {
+	const letters = "0123456789ABCDEF"
+	code := "#"
+	for i := 0; i < 6; i++ {
+		code += string(letters[rand.Intn(len(letters))])
+	}
+	return code
+}
+
+func generateUniqueCode(existingCodes map[string]struct{}, length int) string {
+	var code string
+	for {
+		code = reduceStringLength(faker.CCNumber(), length)
+		if _, exists := existingCodes[code]; !exists {
+			existingCodes[code] = struct{}{}
+			break
+		}
+	}
+	return code
 }
 
 func insertUsers(db *gorm.DB, roles []string) {
-	// insert predefined users
 	predefinedUsers := []struct {
 		Username string
 		Name     string
@@ -47,7 +88,6 @@ func insertUsers(db *gorm.DB, roles []string) {
 
 		password := "password123"
 		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-
 		if err != nil {
 			log.Fatalf("No se pudo encriptar la contraseña: %v", err)
 		}
@@ -60,7 +100,6 @@ func insertUsers(db *gorm.DB, roles []string) {
 		users = append(users, user)
 	}
 
-	// insert standard users
 	for i := 0; i < 2; i++ {
 		user := models.User{
 			ID:       uuid.New().String(),
@@ -71,7 +110,6 @@ func insertUsers(db *gorm.DB, roles []string) {
 
 		password := "password"
 		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-
 		if err != nil {
 			log.Fatalf("No se pudo encriptar la contraseña: %v", err)
 		}
@@ -84,7 +122,6 @@ func insertUsers(db *gorm.DB, roles []string) {
 		users = append(users, user)
 	}
 
-	// batch insert users
 	result := db.Create(&users)
 	if result.Error != nil {
 		log.Fatalf("No se pudo crear los usuarios: %v", result.Error)
@@ -93,11 +130,14 @@ func insertUsers(db *gorm.DB, roles []string) {
 
 func insertColors(db *gorm.DB) {
 	var colors []models.Color
+	uniqueCodes := make(map[string]struct{})
 
 	for i := 0; i < 10; i++ {
+		code := generateUniqueCode(uniqueCodes, 5)
+
 		color := models.Color{
 			Name: faker.Name(),
-			Code: generateColorNumber(),
+			Code: code,
 			Hex:  generateColorCode(),
 		}
 
@@ -114,35 +154,209 @@ func insertColors(db *gorm.DB) {
 	}
 }
 
-func generateColorCode() string {
-	const letters = "0123456789ABCDEF"
-	code := "#"
-	for i := 0; i < 6; i++ {
-		code += string(letters[rand.Intn(len(letters))])
-	}
-	return code
-}
-
-func generateColorNumber() string {
-	const digits = "0123456789"
-	length := rand.Intn(3) + 4 // generates a number between 4 and 6
-	code := ""
-	for i := 0; i < length; i++ {
-		code += string(digits[rand.Intn(len(digits))])
-	}
-	return code
-}
-
 func generateSupplier(db *gorm.DB) {
-	s := models.Supplier{}
-	err := faker.FakeData(&s)
+	var suppliers []models.Supplier
+	uniqueCodes := make(map[string]struct{})
+	uniqueNits := make(map[string]struct{})
 
-	if err != nil {
-		log.Fatalf("No se pudo generar el proveedor: %v", err)
+	for i := 0; i < 10; i++ {
+		code := generateUniqueCode(uniqueCodes, 4)
+		nit := generateUniqueCode(uniqueNits, 9)
+
+		supplier := models.Supplier{
+			Name:  faker.Name(),
+			Brand: faker.LastName(),
+			Nit:   nit,
+			Code:  code,
+		}
+
+		now := time.Now()
+		supplier.CreatedAt = &now
+		supplier.UpdatedAt = &now
+
+		suppliers = append(suppliers, supplier)
 	}
 
-	result := db.Create(&s)
+	result := db.Create(&suppliers)
 	if result.Error != nil {
-		log.Fatalf("No se pudo crear el proveedor: %v", result.Error)
+		log.Fatalf("No se pudo crear los proveedores: %v", result.Error)
 	}
+}
+
+func generateResource(db *gorm.DB) {
+	var resources []models.Resource
+
+	var suppliers []models.Supplier
+	result := db.Find(&suppliers)
+	if result.Error != nil {
+		log.Fatalf("No se pudo obtener los proveedores: %v", result.Error)
+	}
+
+	var colors []models.Color
+	result = db.Find(&colors)
+	if result.Error != nil {
+		log.Fatalf("No se pudo obtener los colores: %v", result.Error)
+	}
+
+	uniqueCodes := make(map[string]struct{})
+
+	for i := 0; i < 10; i++ {
+		code := generateUniqueCode(uniqueCodes, 5)
+
+		resource := models.Resource{
+			Name:       faker.Name(),
+			Cost:       float64(faker.UnixTime()),
+			Code:       code,
+			ColorID:    colors[rand.Intn(len(colors))].ID,
+			SupplierID: suppliers[rand.Intn(len(suppliers))].ID,
+		}
+
+		now := time.Now()
+		resource.CreatedAt = now
+
+		resources = append(resources, resource)
+	}
+
+	result = db.Create(&resources)
+}
+
+func generateComposition(db *gorm.DB) {
+	var compositions []models.Composition
+
+	for i := 0; i < 10; i++ {
+		var algod, elast, lino, nylon, polye, rayon, rayvis, tencel, visco, hilom uint
+		total := 0
+
+		for total < 100 {
+			remaining := 100 - total
+			algod = uint(rand.Intn(remaining + 1))
+			total += int(algod)
+			if total >= 100 {
+				break
+			}
+
+			remaining = 100 - total
+			elast = uint(rand.Intn(remaining + 1))
+			total += int(elast)
+			if total >= 100 {
+				break
+			}
+
+			remaining = 100 - total
+			lino = uint(rand.Intn(remaining + 1))
+			total += int(lino)
+			if total >= 100 {
+				break
+			}
+
+			remaining = 100 - total
+			nylon = uint(rand.Intn(remaining + 1))
+			total += int(nylon)
+			if total >= 100 {
+				break
+			}
+
+			remaining = 100 - total
+			polye = uint(rand.Intn(remaining + 1))
+			total += int(polye)
+			if total >= 100 {
+				break
+			}
+
+			remaining = 100 - total
+			rayon = uint(rand.Intn(remaining + 1))
+			total += int(rayon)
+			if total >= 100 {
+				break
+			}
+
+			remaining = 100 - total
+			rayvis = uint(rand.Intn(remaining + 1))
+			total += int(rayvis)
+			if total >= 100 {
+				break
+			}
+
+			remaining = 100 - total
+			tencel = uint(rand.Intn(remaining + 1))
+			total += int(tencel)
+			if total >= 100 {
+				break
+			}
+
+			remaining = 100 - total
+			visco = uint(rand.Intn(remaining + 1))
+			total += int(visco)
+			if total >= 100 {
+				break
+			}
+
+			remaining = 100 - total
+			hilom = uint(rand.Intn(remaining + 1))
+			total += int(hilom)
+			if total >= 100 {
+				break
+			}
+		}
+
+		composition := models.Composition{
+			Algod:  algod,
+			Elast:  elast,
+			Lino:   lino,
+			Nylon:  nylon,
+			Polye:  polye,
+			Rayon:  rayon,
+			Rayvis: rayvis,
+			Tencel: tencel,
+			Visco:  visco,
+			Hilom:  hilom,
+		}
+
+		compositions = append(compositions, composition)
+	}
+
+	result := db.Create(&compositions)
+	if result.Error != nil {
+		log.Fatalf("No se pudo crear las composiciones: %v", result.Error)
+	}
+}
+
+func generateFabric(db *gorm.DB) {
+	var fabrics []models.Fabric
+
+	var suppliers []models.Supplier
+	result := db.Find(&suppliers)
+	if result.Error != nil {
+		log.Fatalf("No se pudo obtener los proveedores: %v", result.Error)
+	}
+
+	var colors []models.Color
+	result = db.Find(&colors)
+	if result.Error != nil {
+		log.Fatalf("No se pudo obtener los colores: %v", result.Error)
+	}
+
+	var compositions []models.Composition
+	result = db.Find(&compositions)
+	if result.Error != nil {
+		log.Fatalf("No se pudo obtener las composiciones: %v", result.Error)
+	}
+
+	for i := 0; i < 10; i++ {
+		fabric := models.Fabric{
+			Name:          faker.Name(),
+			Cost:          float64(faker.UnixTime()),
+			Code:          faker.YearString(),
+			ColorID:       colors[rand.Intn(len(colors))].ID,
+			SupplierID:    suppliers[rand.Intn(len(suppliers))].ID,
+			CompositionID: compositions[rand.Intn(len(compositions))].ID,
+		}
+
+		now := time.Now()
+		fabric.CreatedAt = &now
+
+		fabrics = append(fabrics, fabric)
+	}
+
+	result = db.Create(&fabrics)
 }
