d8b0dc60f4a92d8aed53f06c1540de7a20f487ff
chris-sanchez098 <christian.valenzuela@correounivalle.edu.co>
2024-09-30 19:31:16 -0500

    test: test confecciontable

---
+++ app/src/modules/taskControl/components/__test__/Confecciontable.test.tsx
@@ -0,0 +1,117 @@
+import { render, screen } from '@solidjs/testing-library';
+import '@testing-library/jest-dom';
+import ConfeccionTable from '../ConfeccionTable';
+
+vi.mock('~/hooks/useSteps', () => ({
+    useSteps: () => ({ getTasksRecord: () => ({ 1: { name: 'Tarea 0' }, 2: { name: 'Tarea 0' } }) }),
+}));
+
+describe('ConfeccionTable', () => {
+    beforeEach(() => {
+        vi.resetAllMocks();
+    });
+
+    it('renders correctly on empty taskcontrols', () => {
+        render(() => <ConfeccionTable taskControls={[]} />);
+        const tableHeader = screen.getByText('No se han encontrado tareas.');
+
+        expect(tableHeader).toBeInTheDocument();
+    });
+
+    it('renders correctly on taskcontrols', () => {
+        render(() => (
+            <ConfeccionTable
+                taskControls={[
+                    {
+                        id: 200,
+                        created_at: '2024-06-11T22:36:52.140901Z',
+                        started_at: '',
+                        finished_at: '',
+                        rejected_at: '',
+                        task_id: 1,
+                        order_id: 2,
+                        order: { color_by_reference: { color_id: 1, reference: { code: '1' } } },
+                    },
+                    {
+                        id: 0,
+                        created_at: '',
+                        started_at: '2024-06-14T22:36:52.140901Z',
+                        finished_at: '',
+                        rejected_at: '',
+                        task_id: 1,
+                        order_id: 3,
+                        order: { color_by_reference: { color_id: 1, reference: { code: '1' } } },
+                    },
+                    {
+                        id: 0,
+                        created_at: '',
+                        started_at: '',
+                        finished_at: '2024-06-13T22:36:52.140901Z',
+                        rejected_at: '',
+                        task_id: 1,
+                        order_id: 4,
+                        order: { color_by_reference: { color_id: 1, reference: { code: '1' } } },
+                    },
+                    {
+                        id: 0,
+                        created_at: '',
+                        started_at: '',
+                        finished_at: '',
+                        rejected_at: '2024-06-12T22:36:52.140901Z',
+                        task_id: 1,
+                        order_id: 5,
+                        order: { color_by_reference: { color_id: 1, reference: { code: '1' } } },
+                    },
+                ]}
+            />
+        ));
+
+        const refTitle = screen.getByText('Referencia');
+        const ordenTitle = screen.getByText('Orden');
+        const IDTitle = screen.getByText('ID');
+        const taskTitle = screen.getByText('Tarea');
+        const stateTitle = screen.getByText('Estado');
+        const createdTitle = screen.getByText('Fecha de creación');
+        const startedTitle = screen.getByText('Fecha de inicio');
+        const ejectedTitle = screen.getByText('Fecha de rechazo');
+        const finishedTitle = screen.getByText('Fecha de finalización');
+        const actionsTitle = screen.getByText('Acciones');
+
+        const taskControlid = screen.getByText('200');
+        const orderId = screen.getByText('5');
+        const stateCreated = screen.getByText('Creada');
+        const stateStarted = screen.getByText('Iniciada');
+        const stateRejected = screen.getByText('Rechazada');
+        const stateFinished = screen.getByText('Terminada');
+        const refValue = screen.getAllByText('1');
+
+        const finishedAt = screen.getByText('2024-06-13 5:36 PM');
+        const rejectedAt = screen.getByText('2024-06-12 5:36 PM');
+        const startedAt = screen.getByText('2024-06-14 5:36 PM');
+        const createdAt = screen.getByText('2024-06-11 5:36 PM');
+
+        expect(refTitle).toBeInTheDocument();
+        expect(ordenTitle).toBeInTheDocument();
+        expect(IDTitle).toBeInTheDocument();
+        expect(taskTitle).toBeInTheDocument();
+        expect(stateTitle).toBeInTheDocument();
+        expect(createdTitle).toBeInTheDocument();
+        expect(startedTitle).toBeInTheDocument();
+        expect(ejectedTitle).toBeInTheDocument();
+        expect(finishedTitle).toBeInTheDocument();
+        expect(actionsTitle).toBeInTheDocument();
+
+        expect(taskControlid).toBeInTheDocument();
+        expect(orderId).toBeInTheDocument();
+        expect(stateCreated).toBeInTheDocument();
+        expect(stateStarted).toBeInTheDocument();
+        expect(stateRejected).toBeInTheDocument();
+        expect(stateFinished).toBeInTheDocument();
+        expect(refValue).length(4);
+
+        expect(finishedAt).toBeInTheDocument();
+        expect(rejectedAt).toBeInTheDocument();
+        expect(startedAt).toBeInTheDocument();
+        expect(createdAt).toBeInTheDocument();
+    });
+});
